214e432a827c41deb4eca8729c87b4f71f4d0ce6
Sally Boulton <p04272494@myemail.dmu.ac.uk>
2016-02-19 23:07:34 +0000

    missing bits

---
+++ Devil Bouncer/Classes/Timer.js
@@ -0,0 +1,17 @@
+﻿
+
+function setTime() {
+    ++totalSeconds;
+    secondsLabel.innerHTML = pad(totalSeconds % 60);
+    minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+    var valString = val + "";
+    if (valString.length < 2) {
+        return "0" + valString;
+    }
+    else {
+        return valString;
+    }
+}
